05259a5d0a7d68d77da73868f84b7a3a5ea952c5
sadakiawada <spark3xxx@gmail.com>
2022-08-22 12:41:51 +0900

    fix: whitelist

--- restfulapicrud/settings.py
+++ restfulapicrud/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
 
-# ALLOWED_HOSTS = ['*', 'https://guluten-free-life-django.herokuapp.com/']
+ALLOWED_HOSTS = ['https://guluten-free-life-django.herokuapp.com/']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
